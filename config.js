e5226b51bf354260b60914f8038780fd800c6de8
xzh.hf <xzh.hf@hangshutech.com>
2020-01-07 18:47:26 +0800

    体验版

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const config = {
-    // api_base_url: "http://bl.7yue.pro/v1/",
-    api_base_url: "http://localhost:3000/v1/",
+    // api_base_url: "http://bl.7yue.pro/v1/", https://island.huangfushengkun.online
+    // api_base_url: "http://localhost:3000/v1/",
+  api_base_url: "https://island.huangfushengkun.online/v1/",
     // appkey: "AbhC31IG7ruCDp57"
 }
